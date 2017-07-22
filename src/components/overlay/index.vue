0b87a94a804555fd4df616e6325e789180b54092
yanm1ng <yummy1121@foxmail.com>
2017-07-22 17:33:24 +0800

    Add component OverLay

---
+++ src/components/overlay/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="yui-overlay" :style="styles" @click="clickHandler">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    opacity: {
+      type: Number,
+      default: 0.35
+    }
+  },
+  computed: {
+    styles () {
+      return {
+        'opacity': this.opacity
+      }
+    }
+  },
+  methods: {
+    clickHandler (e) {
+      this.$emit('click', e)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~styles/variable.scss';
+.yui {
+  &-overlay {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    background: #000;
+    user-select: none;
+  }
+}
+</style>
