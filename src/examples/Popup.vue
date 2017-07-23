5582f6f40e8dc1ceb3c7c06b5ce97b57d4d41696
yanm1ng <yummy1121@foxmail.com>
2017-07-23 15:19:57 +0800

    add Component Popup

---
+++ src/examples/Popup.vue
@@ -0,0 +1,52 @@
+<template>
+  <section :style="{ 'margin': '10px' }">
+    <x-button @on-click="handlePopup('bottom')">Open Popup Bottom</x-button>
+    <x-button @on-click="handlePopup('top')">Open Popup Top</x-button>
+    <x-button @on-click="handlePopup('left')">Open Popup Left</x-button>
+    <x-button @on-click="handlePopup('center')">Open Popup Center</x-button>
+    <x-button @on-click="handlePopup('full')">Open Popup Full</x-button>
+    <popup :open="bottom" @on-close="handlePopup('bottom')">
+      <div>hello</div>
+    </popup>
+    <popup :open="top" direction="top" @on-close="handlePopup('top')">
+      <div>hello</div>
+    </popup>
+    <popup :open="left" direction="left" @on-close="handlePopup('left')">
+      <div>hello</div>
+    </popup>
+    <popup :open="center" direction="center" @on-close="handlePopup('center')">
+      <div>hello</div>
+    </popup>
+    <popup :open="full" direction="bottom" :full="true" @on-close="handlePopup('full')">
+      <div>hello</div>
+    </popup>
+  </section>
+</template>
+
+<script>
+import {
+  XButton,
+  Popup
+} from 'components'
+
+export default {
+  components: {
+    XButton,
+    Popup
+  },
+  data() {
+    return {
+      bottom: false,
+      top: false,
+      left: false,
+      center: false,
+      full: false
+    }
+  },
+  methods: {
+    handlePopup(direction) {
+      this[direction] = !this[direction]
+    }
+  }
+}
+</script>
