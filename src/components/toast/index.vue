ef61cc67ad8b416398c03300489584829b217121
yanm1ng <yummy1121@foxmail.com>
2017-07-24 15:58:24 +0800

    add Component Toast

---
+++ src/components/toast/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="classes">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'toast',
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    direction: {
+      type: String,
+      default: 'center'
+    },
+    time: {
+      type: Number,
+      default: 3000
+    }
+  },
+  computed: {
+    classes() {
+      return [
+        'yui-toast',
+        `yui-toast-${this.direction}`
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/examples/Toast.vue
@@ -0,0 +1,22 @@
+<template>
+  <section :style="{ 'margin': '10px' }">
+    <x-button @on-click="handleToast('bottom')">Open Toast Bottom</x-button>
+  </section>
+</template>
+
+<script>
+import {
+  XButton
+} from 'components'
+
+export default {
+  components: {
+    XButton
+  },
+  methods: {
+    handleToast() {
+
+    }
+  }
+}
+</script>
