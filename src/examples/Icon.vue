a80973c352d54d02776028fa9e3345c76f6714dc
yanm1ng <yummy1121@foxmail.com>
2017-07-24 16:20:03 +0800

    Update Icon examples

--- src/examples/Icon.vue
+++ src/examples/Icon.vue
@@ -1,7 +1,11 @@
 <template>
   <section>
-    <icon />
-    <icon name="check" />
+    <ul class="icon-list">
+      <li v-for="icon in icons" :key="icon">
+        <icon :name="icon"></icon>
+        <div>{{ icon }}</div>
+      </li>
+    </ul>
   </section>
 </template>
 
@@ -11,6 +15,33 @@ import { Icon } from 'components'
 export default {
   components: {
     Icon
+  },
+  data() {
+    return {
+      icons: [
+        'check', 'close', 'emoji', 'favorfill', 'favor', 'loading', 'roundcheckfill', 'roundcheck', 'roundclosefill', 'roundclose', 'roundrightfill', 'roundright', 'search', 'timefill', 'time', 'unfold', 'warnfill', 'warn', 'likefill', 'like', 'order', 'back', 'more', 'scan', 'questionfill', 'question', 'top', 'pulldown', 'pullup', 'right', 'refresh', 'deletefill', 'delete', 'home', 'homefill', 'squarecheckfill', 'square', 'squarecheck', 'round', 'roundaddfill', 'roundadd', 'add', 'share'
+      ]
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.icon-list {
+  list-style: none;
+  width: 100%;
+  li {
+    font-size: 20px;
+    float: left;
+    text-align: center;
+    list-style: none;
+    margin: 10px;
+    width: 72px;
+    div {
+      color: #666;
+      font-size: 14px;
+      line-height: 1.8;
+    }
+  }
+}
+</style>
