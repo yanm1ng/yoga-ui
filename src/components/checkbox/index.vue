c8569fb09263aec4a9b9d06a558267225c5c9145
yanm1ng <yummy1121@foxmail.com>
2017-07-28 09:43:24 +0800

    add Component Checkbox

---
+++ src/components/checkbox/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <cell-box :title="title">
+    <cell v-for="checkbox in options" :title="checkbox.label" :key="checkbox.value" @on-click="onItemClick(checkbox.value)">
+      <div slot="value" :class="classes(checkbox.value)">
+        <icon name="check"></icon>
+      </div>
+    </cell>
+  </cell-box>
+</template>
+
+<script>
+import Cell from '../cell'
+import CellBox from '../cell-box'
+import Icon from '../icon'
+
+export default {
+  name: 'checkbox',
+  props: {
+    title: String,
+    values: {
+      type: Array,
+      default: []
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    max: Number
+  },
+  data() {
+    return {
+      currentValues: []
+    }
+  },
+  created() {
+    if (this.values) {
+      this.currentValues = this.values
+    }
+  },
+  computed: {
+    _max() {
+      const { options, max } = this
+      const total = options.length
+      if (max) {
+        if (max > total) {
+          return total
+        }
+        return max
+      } else {
+        return total
+      }
+    }
+  },
+  methods: {
+    classes(val) {
+      return [
+        'yui-checkbox',
+        {
+          'yui-checkbox-checked': this.currentValues.indexOf(val) !== -1
+        }
+      ]
+    },
+    onItemClick(val) {
+      const index = this.currentValues.indexOf(val)
+      if (index === -1) {
+        (this._max > this.currentValues.length) && this.currentValues.push(val)
+      } else {
+        this.currentValues.splice(index, 1)
+      }
+      this.$emit('on-change', pure(this.currentValues))
+    }
+  },
+  components: {
+    Cell,
+    CellBox,
+    Icon
+  }
+}
+
+function pure(obj) {
+  return JSON.parse(JSON.stringify(obj))
+}
+</script>
+
+<style lang="scss">
+@import '~styles/variable.scss';
+
+.yui {
+  &-checkbox {
+    width: 20px;
+    height: 20px;
+    position: relative;
+    .yui-iconfont {
+      display: none;
+    }
+    &-checked {
+      .yui-iconfont {
+        position: absolute;
+        top: -6px;
+        left: -2px;
+        display: block;
+        font-size: 24px;
+        color: $primary-color;
+      }
+    }
+  }
+}
+</style>
---
+++ src/examples/Checkbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <checkbox title="选择题" :max="2" :values="values" :options="options" @on-change="handleChange"></checkbox>
+  </section>
+</template>
+
+<script>
+import { Checkbox } from 'components'
+
+export default {
+  data() {
+    return {
+      values: ['1'],
+      options: [
+        { label: '选项一', value: '1' },
+        { label: '选项二', value: '2' },
+        { label: '选项三', value: '3' }
+      ]
+    }
+  },
+  methods: {
+    handleChange(values) {
+      console.log(values)
+    }
+  },
+  components: {
+    Checkbox
+  }
+}
+</script>
