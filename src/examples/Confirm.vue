2bd0caf19aecde02b98ed27f6e886db58872d59f
yanm1ng <yummy1121@foxmail.com>
2017-07-30 15:03:45 +0800

    Add confirm examples

--- src/examples/Confirm.vue
+++ src/examples/Confirm.vue
@@ -1,9 +1,11 @@
 <template>
   <section>
     <cell-box title="Alert">
-      <x-switch v-model="open" title="normal"></x-switch>
+      <x-switch v-model="open1" title="default"></x-switch>
+      <x-switch v-model="open2" title="footer-text"></x-switch>
     </cell-box>
-    <confirm v-model="open" content="你好吗？" title="温馨提示" confirm-text="还不错" @on-show="onShow" @on-confirm="onConfirm"></confirm>
+    <confirm v-model="open1" content="你好吗？" title="温馨提示" @on-show="onShow" @on-confirm="onConfirm"></confirm>
+    <confirm v-model="open2" content="你好吗？" title="温馨提示" cancel-text="不好" confirm-text="还不错" @on-show="onShow" @on-confirm="onConfirm"></confirm>
   </section>
 </template>
 
@@ -24,13 +26,11 @@ export default {
   },
   data() {
     return {
-      open: false
+      open1: false,
+      open2: false
     }
   },
   methods: {
-    handleChange(val) {
-      this.open = val
-    },
     onShow() {
       console.log('on-show')
     },
