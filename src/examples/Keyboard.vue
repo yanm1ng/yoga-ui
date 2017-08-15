56dd253207a6f72c9914573328411040114c7e18
yanm1ng <yummy1121@foxmail.com>
2017-08-15 14:49:52 +0800

    Add component keyboard

---
+++ src/examples/Keyboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <cell-box>
+      <x-input title="姓名" v-model="value" placeholder="你叫什么"></x-input>
+    </cell-box>
+    <keyboard v-model="value"></keyboard>
+  </section>
+</template>
+
+<script>
+import {
+  Keyboard,
+  CellBox,
+  XInput
+} from 'components'
+
+export default {
+  data() {
+    return {
+      value: ''
+    }
+  },
+  components: {
+    Keyboard,
+    CellBox,
+    XInput
+  }
+}
+</script>
