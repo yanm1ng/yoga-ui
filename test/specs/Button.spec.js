5c9625666eb8aa9206a707c1ecd2f0ec40e568bf
yanm1ng <yummy1121@foxmail.com>
2017-10-09 12:44:41 +0800

    Update button unit test with click and props render test

--- test/specs/Button.spec.js
+++ test/specs/Button.spec.js
@@ -1,17 +1,54 @@
-import Vue from 'vue'
-import { getRenderedVm } from '../utils'
+import { getRenderedVm, createVue } from '../utils'
 import Button from 'components/button'
 
-describe('Button.vue', () => {
+describe('Component button testing', () => {
   it('should render correct classes', () => {
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor().$mount()
+    let vm = getRenderedVm(Button, {})
     expect(vm.$el.className).toEqual('yui-btn yui-btn-primary')
   })
-  it('should render correct props', () => {
+  it('should render props:type', () => {
+    let vm = getRenderedVm(Button, {
+      type: 'ghost'
+    })
+    expect(vm.type).toEqual('ghost')
+    expect(vm.$el.className).toEqual('yui-btn yui-btn-ghost')
+  })
+  it('should render props:size', () => {
     let vm = getRenderedVm(Button, {
       size: 'mini'
     })
     expect(vm.size).toEqual('mini')
+    expect(vm.$el.classList.contains('yui-btn-mini')).toEqual(true)
+  })
+  it('should render props:circle', () => {
+    let vm = getRenderedVm(Button, {
+      circle: true
+    })
+    expect(vm.circle).toEqual(true)
+    expect(vm.$el.classList.contains('yui-btn-circle')).toEqual(true)
+  })
+  it('should render props:disable', () => {
+    let vm = getRenderedVm(Button, {
+      disabled: true
+    })
+    expect(vm.$el.disabled).toEqual(true)
+    expect(vm.$el.classList.contains('yui-btn-disabled')).toEqual(true)
+  })
+  it('click', () => {
+    let result = null
+    let vm = createVue({
+      template: `
+        <x-button @on-click="handleClick"></x-button>
+      `,
+      methods: {
+        handleClick(e) {
+          result = e
+        }
+      }
+    })
+    vm.$el.click()
+    setTimeout(() => {
+      expect(result).not.toBeNull()
+    }, 20)
   })
 })
--- test/utils.js
+++ test/utils.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
+import yogaUI from 'src'
+
+Vue.use(yogaUI)
+
+let id = 0
+const createElm = function() {
+  const elm = document.createElement('div')
+  elm.id = 'app' + ++id
+  document.body.appendChild(elm)
+  return elm
+}
 
 export function getRenderedVm(Component, propsData) {
-  const Ctor = Vue.extend(Component)
-  const vm = new Ctor({ propsData }).$mount()
+  const Constructor = Vue.extend(Component)
+  const vm = new Constructor({ propsData }).$mount()
   return vm
+}
+
+export function createVue(Component) {
+  if (Object.prototype.toString.call(Component) === '[object String]') {
+    Component = { template: Component }
+  }
+  return new Vue(Component).$mount(createElm())
 }
