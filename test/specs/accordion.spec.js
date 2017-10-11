8caea1698a252622781f6d7a9ab2282abcbddd9d
yanm1ng <yummy1121@foxmail.com>
2017-10-11 12:55:45 +0800

    Added component actionsheet and accrodion unit test

---
+++ test/specs/accordion.spec.js
@@ -0,0 +1,100 @@
+import { getRenderedVm, createVue } from '../utils'
+import Accordion from 'components/accordion'
+
+describe('component accordion testing', () => {
+  it('should render correct classes', () => {
+    let vm = getRenderedVm(Accordion, {
+      list: [
+        { title: 'title1', content: 'content1' },
+        { title: 'title2', content: 'content2' }
+      ]
+    })
+    expect(vm.$el.className).toEqual('yui-cells-box')
+    expect(vm.$el.querySelectorAll('.yui-accordion').length).toEqual(2)
+  })
+  it('should render props:defaultOpened', () => {
+    let vm = getRenderedVm(Accordion, {
+      list: [
+        { title: 'title1', content: 'content1' },
+        { title: 'title2', content: 'content2' }
+      ],
+      defaultOpened: [0, 1]
+    })
+    expect(vm.defaultOpened.length).toEqual(2)
+    expect(vm.$el.querySelectorAll('.yui-cell-active').length).toEqual(2)
+  })
+  it('should render props:animate', () => {
+    let vm = getRenderedVm(Accordion, {
+      list: [
+        { title: 'title1', content: 'content1' },
+        { title: 'title2', content: 'content2' }
+      ],
+      animate: false
+    })
+    expect(vm.animate).toEqual(false)
+    expect(vm.$el.querySelector('.no-animate')).not.toBeNull()
+  })
+  it('should render props:single', () => {
+    let vm = getRenderedVm(Accordion, {
+      list: [
+        { title: 'title1', content: 'content1' },
+        { title: 'title2', content: 'content2' }
+      ],
+      single: true
+    })
+    expect(vm.single).toEqual(true)
+  })
+  it('click', () => {
+    let result = null
+    let vm = createVue({
+      template: `
+        <accordion title="title" :list="list" @on-change="handleChange"></accordion>
+      `,
+      data() {
+        return {
+          list: [
+            { title: 'title1', content: 'content1' },
+            { title: 'title2', content: 'content2' }
+          ]
+        }
+      },
+      methods: {
+        handleChange(values) {
+          result = values
+        }
+      }
+    })
+    vm.$el.querySelector('.yui-cell-arrow').click()
+    setTimeout(() => {
+      expect(result).not.toBeNull()
+    }, 200)
+  })
+  it('click single', () => {
+    let result = null
+    let vm = createVue({
+      template: `
+        <accordion title="title" :single="true" :list="list" @on-change="handleChange"></accordion>
+      `,
+      data() {
+        return {
+          list: [
+            { title: 'title1', content: 'content1' },
+            { title: 'title2', content: 'content2' }
+          ]
+        }
+      },
+      methods: {
+        handleChange(values) {
+          result = values
+        }
+      }
+    })
+    const accordions = vm.$el.querySelectorAll('.yui-cell-arrow')
+    accordions[0].click()
+    accordions[1].click()
+    setTimeout(() => {
+      expect(result).not.toBeNull()
+      expect(result.length).toEqual(1)
+    }, 200)
+  })
+})
