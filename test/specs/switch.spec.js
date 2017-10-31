aa647828c9d846943b0849a6f4c3c16e9306f284
yanm1ng <yummy1121@foxmail.com>
2017-10-31 11:19:06 +0800

    Added component switch unit test

---
+++ test/specs/switch.spec.js
@@ -0,0 +1,70 @@
+import { getRenderedVm, createVue } from '../utils'
+import Switch from 'components/switch'
+
+describe('component switch testing', () => {
+  it('should render correct class', () => {
+    let vm = getRenderedVm(Switch, {})
+    expect(vm.$el.querySelector('input').type).toEqual('checkbox')
+    expect(vm.$el.querySelector('.yui-switch')).not.toBeNull()
+  })
+  it('should render props:title', () => {
+    let vm = createVue({
+      template: `
+        <cell-box title="switch">
+          <x-switch v-model="value" title="title"></x-switch>
+        </cell-box>
+      `,
+      data() {
+        return {
+          value: false
+        }
+      }
+    })
+    expect(vm.$el.querySelector('.yui-label').textContent).toEqual('title')
+  })
+  it('should render props:desc', () => {
+    let vm = createVue({
+      template: `
+        <cell-box title="switch">
+          <x-switch v-model="value" title="title" desc="desc"></x-switch>
+        </cell-box>
+      `,
+      data() {
+        return {
+          value: false
+        }
+      }
+    })
+    expect(vm.$el.querySelector('.yui-label-desc').textContent).toEqual('desc')
+  })
+  it('should render props:value', () => {
+    let vm = createVue({
+      template: `
+        <cell-box title="switch">
+          <x-switch v-model="value" title="title" desc="desc"></x-switch>
+        </cell-box>
+      `,
+      data() {
+        return {
+          value: true
+        }
+      }
+    })
+    expect(vm.$el.querySelector('.yui-switch').value).toEqual('on')
+  })
+  it('should render props:disabled', () => {
+    let vm = createVue({
+      template: `
+        <cell-box title="switch">
+          <x-switch v-model="value" title="title" :disabled="true"></x-switch>
+        </cell-box>
+      `,
+      data() {
+        return {
+          value: true
+        }
+      }
+    })
+    expect(vm.$el.querySelector('.yui-switch').getAttribute('disabled')).toEqual('disabled')
+  })
+})
