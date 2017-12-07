2b7b84dcb8d6cf367b5d7bb9e2c34ffe6fc67650
yanm1ng <i@yanm1ng.cn>
2017-12-07 20:50:54 +0800

    feat:add component button-tab example

--- examples/pages/ButtonTab.vue
+++ examples/pages/ButtonTab.vue
@@ -6,6 +6,12 @@
       <button-tab-item>热门</button-tab-item>
       <button-tab-item>最近</button-tab-item>
     </button-tab>
+    <white-space />
+    <button-tab :default-value="1" @on-change="handleChange">
+      <button-tab-item>全部</button-tab-item>
+      <button-tab-item>热门</button-tab-item>
+      <button-tab-item>最近</button-tab-item>
+    </button-tab>
   </wing-blank>
 </template>
 
